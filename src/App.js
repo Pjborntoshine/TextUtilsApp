61be8783a5f0bef6181ee9ebf204a83674f88b6c
pjborntoshine <prajwaljawanjalj@gmail.com>
2023-08-08 14:34:44 +0530

    added about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextFrom from './components/TextFrom'; 
+// import About from "./components/About";
+ import TextFrom from './components/TextFrom'; 
 
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" AboutText="About" />
-      <div className="container my-3"><TextFrom heading="Enter the text to enalyze below"/></div>
-      
+      <Navbar title="TextUtils" AboutText="About"/>
+       <div className="container my-3"><TextFrom heading="Enter the text to enalyze below"/></div> 
+      {/* <div className="container">
+        <About/>
+      </div> */}
       
     </>
   );
--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -1,27 +1,64 @@
 import React, { useState } from 'react';
 
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
 
-export default function TextFrom(props) {
-  const handelUpClick = ()=>{
-    //console.log("Uppercase was clicked" + text);
-    let newText=text.toUpperCase();
-    setText(newText)
-  }
-  const handelOnChange = (event)=>{
-    // console.log("on change");
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleClearClick = () => {
+    setText('');
+  };
+
+  const handleOnChange = (event) => {
     setText(event.target.value);
-  }
-  const [text, setText] = useState('Enter text here...');
+  };
+
+  const [text, setText] = useState('');
+
+  // Calculate reading time in minutes (approximation)
+  const readingTime = Math.ceil(text.split(/\s+/).filter(Boolean).length * 0.008);
 
   return (
-<div>
-<h1>{props.heading }</h1>
-<div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handelOnChange} id="text-box" rows="8"></textarea>
-  <button className="btn btn-primary"on onClick={handelUpClick}>convert to Uppercase</button>
-</div>
-    
-</div>
-  )
+    <>
+    <div className="container1">
+      <div className='container'>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="text-box"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>&nbsp;&nbsp;
+        <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>&nbsp;&nbsp;
+        <button className="btn btn-primary" onClick={handleClearClick}>Clear Text</button>
+      </div>
+      <div className="container my-2">
+        <h2 style={{ color: "skyblue" }}>Your Text Summary</h2>
+        <p>
+          <b>
+            {text.split(/\s+/).filter(Boolean).length} words and&nbsp;
+            {text.replace(/\s+/g, "").length} characters
+          </b>
+        </p>
+        <p>
+          {readingTime} minutes to read
+        </p>
+        <h3>Preview</h3>
+        <p>
+          {text}
+        </p>
+      </div>
+      </div>
+    </>
+  );
 }
-
